cace3d6000c37918ede4750dcd77d5f20c772b24
yan <wyvernnot@gmail.com>
2016-12-05 15:36:01 +0800

    add coverage test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "env": {
+    "jest": true
+  }
 };
---
+++ src/__tests__/QiniuPlugin.js
@@ -0,0 +1,8 @@
+import QiniuPlugin from '../QiniuPlugin';
+
+describe('QiniuPlugin', () => {
+  it('works', () => {
+    const plugin = new QiniuPlugin();
+    expect(plugin);
+  });
+});
